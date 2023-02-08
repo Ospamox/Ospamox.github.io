f64cc8eb40eab3838f40615555102d19ce424306
Ospamox <71867223+Ospamox@users.noreply.github.com>
2023-02-08 10:09:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,14 +12,17 @@ let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
+
 let btn6 = document.getElementById("btn6");
+
+
 
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 10000000000000000000!");
+		tg.MainButton.setText("Вы выбрали товар 1!");
 		item = "1";
 		tg.MainButton.show();
 	}
@@ -61,24 +64,58 @@ btn4.addEventListener("click", function(){
 btn5.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
+
 	}
 	else {
+
 		tg.MainButton.setText("Вы выбрали товар 5!");
 		item = "5";
 		tg.MainButton.show();
 	}
 });
 
-btn6.addEventListener("click", function(){
+
+btnmin.addEventListener("click", function(){
+
+	// this.remove()
+	// console.log('Hello, World!');
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
+		console.log('Hello, World!');
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
+		btn0.innerHTML = btn0.innerHTML - 1;
+		if (btn0.innerHTML < 0)
+		{
+			btn0.innerHTML = 0
+		}
+        console.log(btn0.innerHTML)
+        sum = btn0.innerHTML*310
+        console.log("Вы заказали Биг Тейсти в количестве " + btn0.innerHTML +" "+"на суму "+ sum )
+		// tg.MainButton.setText("Вы выбрали товар 6!");
+		// item = "6";
+		// tg.MainButton.show();
+	}
+ 	
+});
+
+btnplus.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+
+	}
+	else {
+          btn0.innerHTML = +btn0.innerHTML + 1;
+          console.log(btn0.innerHTML)
+          sum = btn0.innerHTML*310
+          console.log("Вы заказали Биг Тейсти в количестве " + btn0.innerHTML +" "+"на суму "+ sum )
+               
+		tg.MainButton.setText("Вы выбрали товар 5!");
+		item = "5";
 		tg.MainButton.show();
 	}
 });
+
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){

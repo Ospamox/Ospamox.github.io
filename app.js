7b50d41df0fbf20d3d271b4d4a9b8b7154cd5c14
Ospamox <71867223+Ospamox@users.noreply.github.com>
2023-03-07 20:46:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -323,7 +323,8 @@ let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+${tg.initDataUnsafe.user.last_name}
+${tg.initDataUnsafe.user.id}`;
 
 
 usercard.appendChild(p); 

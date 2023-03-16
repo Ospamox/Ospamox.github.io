71da347bf612c06c99a84a281e40a0c8fa519be5
Ospamox <71867223+Ospamox@users.noreply.github.com>
2023-03-16 11:49:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -312,6 +312,25 @@ btnplus.addEventListener("click", function(){
 // 	}
 });
 
+let open_modal = document.querySelectorAll('.open_modal');
+let close_modal = document.getElementById('close_modal');
+let modal = document.getElementById('modal');
+let body = document.getElementsByTagName('body')[0];
+for (let i = 0; i < open_modal.length; i++) {
+    open_modal[i].onclick = function() { // клик на открытие
+        modal.classList.add('modal_vis'); // добавляем видимость окна
+        modal.classList.remove('bounceOutDown'); // удаляем эффект закрытия
+        body.classList.add('body_block'); // убираем прокрутку
+    };
+}
+close_modal.onclick = function() { // клик на закрытие
+    modal.classList.add('bounceOutDown'); // добавляем эффект закрытия
+    window.setTimeout(function() { // удаляем окно через полсекунды (чтобы увидеть эффект закрытия).
+        modal.classList.remove('modal_vis');
+        body.classList.remove('body_block'); // возвращаем прокрутку
+    }, 500);
+};
+
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 // 	all = JSON.stringify(all);
